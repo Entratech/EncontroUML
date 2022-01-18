22575b919639866a7df90e8aea59b9543447c4b4
João Vitor <jjvitor44@gmail.com>
2022-01-18 14:30:51 -0300

    Classes, atributos e metodos

---
+++ PessoaFisica.cs
@@ -0,0 +1,10 @@
+namespace Encontroremoto
+{
+    public class PessoaFisica : Pessoa
+    {
+        public string cpf { get; set; }
+
+        public DateTime datanascimento { get; set; }
+
+    }
+}
---
+++ PessoaJuridica.cs
@@ -0,0 +1,13 @@
+namespace Encontroremoto
+{
+    public class PessoaJuridica : Pessoa
+    {
+        public string cnpj { get; set; }
+        
+        public string razaoSocial { get; set; }
+        
+        
+        
+        
+    }
+}
