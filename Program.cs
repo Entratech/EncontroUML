a32938451ab4bcea1d8e52f4076f7129c811882c
unknown <jjvitor44@gmail.com>
2022-02-14 20:40:48 -0300

    Validação

--- Program.cs
+++ Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Encontroremoto
+{
+    class Program
+    {   
+static void Main(string[] args)
+        {
+            // Endereco end = new Endereco();
+            // end.logradouro = "X";
+            // end.numero = 100;
+            // end.complemento = "Próximo ao Senai";
+            // end.enderecoComercial = false;
+
+            // PessoaFisica novapf = new PessoaFisica();
+
+            // novapf.endereco = end;
+            // novapf.cpf = "123456789";
+            // novapf.dataNascimento = new DateTime(2001,08,11);
+            // novapf.nome = "João";
+
+            // PessoaFisica pf = new PessoaFisica();
+            // bool idadeValida = pf.ValidarDataNascimento(novapf.dataNascimento);
+
+            // if (idadeValida == true)
+            // {
+            //     Console.WriteLine($"Cadastro Aprovado.");
+            // }
+            // else
+            // {
+            //     Console.WriteLine($"Cadastro Não Aprovado.");
+            // }
+            
+            // pf.ValidarDataNascimento(pf.dataNascimento);
+            // Console.WriteLine(pf.ValidarDataNascimento(novapf.dataNascimento));
+            
+            // Console.WriteLine(novapf.endereco.logradouro);
+            // Console.WriteLine(novapf.endereco.numero);
+            // Console.WriteLine(novapf.endereco.complemento);
+            // Console.WriteLine(novapf.endereco.enderecoComercial);
+            // Console.WriteLine($"Rua: {novapf.endereco.logradouro}, {novapf.endereco.numero}");
+            // Console.WriteLine("Rua: " + novapf.endereco.logradouro + novapf.endereco.numero)
+
+            Endereco end = new Endereco();
+
+            end.logradouro = "Rua Salvador Bercari";
+            end.numero = 7;
+            end.complemento = "Na rua da auto-escola";
+            end.enderecoComercial = false;
+
+            PessoaFisica novapf = new PessoaFisica();
+
+            novapf.endereco = end;
+            novapf.cpf="123456789";
+            novapf.dataNascimento = new DateTime(2001,08,11);
+            novapf.nome = "Daniel Josua";
+
+            PessoaFisica pf = new PessoaFisica();
+            pf.ValidarDataNascimento(pf.dataNascimento);
+
+            bool idadeValida = pf.ValidarDataNascimento(novapf.dataNascimento);
+            Console.WriteLine(idadeValida);
+
+            if(idadeValida == true)
+            {
+                Console.WriteLine($"Cadastro aprovado");
+            }
+            else
+            {
+                Console.WriteLine($"Cadastro recusado");
+            }
+
+  }
+
+    }
+        }
+
