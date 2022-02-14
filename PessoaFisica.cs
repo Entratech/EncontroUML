b4559f4a1a0fbb174e0217364142936c64a7bd05
unknown <jjvitor44@gmail.com>
2022-02-14 20:08:06 -0300

    Atribuindo CPF/data de nascimento para pessoa física

--- PessoaFisica.cs
+++ PessoaFisica.cs
@@ -2,9 +2,23 @@ namespace Encontroremoto
 {
     public class PessoaFisica : Pessoa
     {
-        public string cpf { get; set; }
+        public string? cpf { get; set; }
 
-        public DateTime datanascimento { get; set; }
+        public DateTime dataNascimento { get; set; }
+        public override void PagarImposto(float salario)
+        {
+            
+        }
+        public bool ValidarDataNascimento(DateTime dataNasc){
+            DateTime dataAtual =DateTime.Today;
 
+            double anos = (dataAtual - dataNasc).TotalDays / 365;
+
+            if(anos<=18){
+                return true;
+            }else{
+                return false;
+            }
+        }
     }
 }
--- PessoaJuridica.cs
+++ PessoaJuridica.cs
@@ -2,9 +2,13 @@ namespace Encontroremoto
 {
     public class PessoaJuridica : Pessoa
     {
-        public string cnpj { get; set; }
+        public string? cnpj { get; set; }
         
-        public string razaoSocial { get; set; }
+        public string? razaoSocial { get; set; }
+
+        public override void PagarImposto(float salario){
+
+        }
         
         
         
